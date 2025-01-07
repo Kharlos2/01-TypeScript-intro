9cfc73fb85c42371116716a5abfc35931d4458bb
carlos.riveram <carlos.riveram@pragma.com.co>
2025-01-06 21:57:59 -0500

    add Pokémon data and update greeting in main app

---
+++ src/bases/02-objects.ts
@@ -0,0 +1,8 @@
+export const pokemonsIds: number[] = [1, 2, 3, 4, 5];
+
+export const pokemon = {
+    id: 1,
+    name: 'Pikachu',
+}
+
+console.log(pokemon);
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import './style.css'
 import typescriptLogo from './typescript.svg'
 import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.ts'
+import { pokemon, pokemonsIds } from './bases/02-objects.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div>
@@ -11,7 +12,7 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     <a href="https://www.typescriptlang.org/" target="_blank">
       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
     </a>
-    <h1>Vite + TypeScript</h1>
+    <h1>Hola ${ pokemon.name }</h1>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
